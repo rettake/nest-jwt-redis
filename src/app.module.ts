048eb4d4a0c0f2fee31f81e4dc62d8614f97990b
Vladislav Okhotnikov <rettakecsgo@gmail.com>
2024-09-14 00:16:31 +0700

    docker compose configuration for postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
       entities: [],
       synchronize: true,
     }),
